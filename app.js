72800c85b34fcac97f79738f33dc46f3b2f94e0b
Ewerton <ewerton.silva07@hotmail.com>
2023-11-17 15:55:10 -0300

    create a separate function to use in both scores, and create the event for the reset button

--- app.js
+++ app.js
@@ -2,39 +2,54 @@ let playerOneScore = document.querySelector('#scoreOne');
 let playerTwoScore = document.querySelector('#scoreTwo');
 const buttonPlayerOne = document.querySelector('#playerOne');
 const buttonPlayerTwo = document.querySelector('#playerTwo');
+const resetButton = document.querySelector('#reset');
 
 buttonPlayerOne.addEventListener('click', () => {
-    switch(playerOneScore.innerText) {
+    addPoints(playerOneScore);
+})
+
+buttonPlayerTwo.addEventListener('click', () => {
+    addPoints(playerTwoScore);
+})
+
+resetButton.addEventListener('click', () => {
+    playerOneScore.innerText = '0';
+    playerTwoScore.innerText = '0';
+})
+
+function addPoints(score) {
+    switch (score.innerText) {
         case '0':
-            playerOneScore.innerText = '1';
+            score.innerText = '1';
             break;
         case '1':
-            playerOneScore.innerText = '2';
+            score.innerText = '2';
             break;
         case '2':
-            playerOneScore.innerText = '3';
+            score.innerText = '3';
             break;
         case '3':
-            playerOneScore.innerText = '4';
+            score.innerText = '4';
             break;
         case '4':
-            playerOneScore.innerText = '5';
-            break;          
+            score.innerText = '5';
+            break;
         case '5':
-            playerOneScore.innerText = '6';
-            break;          
+            score.innerText = '6';
+            break;
         case '6':
-            playerOneScore.innerText = '7';
-            break;          
+            score.innerText = '7';
+            break;
         case '7':
-            playerOneScore.innerText = '8';
-            break;          
+            score.innerText = '8';
+            break;
         case '8':
-            playerOneScore.innerText = '9';
-            break;          
+            score.innerText = '9';
+            break;
         case '9':
-            playerOneScore.innerText = '10';
-            break;          
+            score.innerText = '10';
+            break;
     }
-})
+}
+
 
